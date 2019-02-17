56df3a90edd6846c22e1ba15477d860943d8a0f9
Dan Nichols <daniel.g.nichols@gmail.com>
2019-02-17 15:17:09 -0800

    Create get-set-data.py

---
+++ get-set-data.py
@@ -0,0 +1,7 @@
+
+
+# Input flags:
+# url:    url of a set on cardgamedb, e.g.
+#         'http://www.cardgamedb.com/index.php/arkhamhorror/arkham-horror-the-card-game/_/guardians-of-the-abyss/'
+
+# Output:
