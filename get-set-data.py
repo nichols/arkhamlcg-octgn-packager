cd52a13ce9d95fe9a90963d11a753a55e48c0d94
Dan Nichols <daniel.g.nichols@gmail.com>
2019-02-17 16:21:29 -0800

    Update get-set-data.py

--- get-set-data.py
+++ get-set-data.py
@@ -1,7 +1,22 @@
 
 
-# Input flags:
-# url:    url of a set on cardgamedb, e.g.
+# Input:  url of a set on cardgamedb, e.g.
 #         'http://www.cardgamedb.com/index.php/arkhamhorror/arkham-horror-the-card-game/_/guardians-of-the-abyss/'
-
-# Output:
+# 
+# Output: 'created <set-name>.json'
+#
+# Creates a .json file containing all the cards in the set.
+# Fields:
+# Card Number
+# Quantity
+# Type
+# Class
+# Level
+# Cost
+# Intellect
+# Combat
+# Skill Icons
+# Traits
+# Text
+# Keywords
+# 
